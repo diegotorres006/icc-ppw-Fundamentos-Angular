b9f58fe0141a9bbe1d956566281008b8bdb403e5
diegotorres006 <jtorresm14@est.ups.edu.ec>
2025-11-07 20:13:05 -0500

    Initial Angular project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
-// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { routes } from './app/app.routes';
 
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(routes, withHashLocation())
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
